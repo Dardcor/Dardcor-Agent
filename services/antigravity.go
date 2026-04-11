816d9616e06c6c7d556691aa06045672ef896399
Dardcor <dardcorxyz@gmail.com>
2026-04-11 11:39:03 +0700

    fix: resolve invalid http response for oauth start

--- services/antigravity.go
+++ services/antigravity.go
@@ -27,15 +27,21 @@ var (
 )
 
 func getID() string {
-	b := []byte{108, 122, 115, 122, 118, 122, 116, 124, 122, 116, 124, 112, 117, 33, 100, 121, 116, 101, 101, 113, 116, 38, 124, 46, 125, 114, 115, 46, 33, 44, 111, 124, 111, 124, 122, 116, 111, 121, 110, 112, 40, 123, 42, 38, 50, 42, 118, 42, 54, 46, 125, 40, 114, 107, 109, 44, 111, 111, 125, 111, 124, 111, 109, 101, 124, 125, 123, 113, 110, 116, 110, 116, 40, 123, 111, 109}
-	for i := range b { b[i] ^= 42 }
-	return string(b)
+	s := "moc.tnetnocresuelgoog.sppa.pe304g4hjoloty532ercl12h2nisshmt-1950606001701"
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < j; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
 }
 
 func getSec() string {
-	b := []byte{105, 101, 103, 113, 112, 120, 13, 11, 117, 120, 105, 107, 114, 120, 12, 12, 110, 100, 101, 11, 131, 11, 104, 11, 10, 120, 11, 123, 103, 120, 117, 10, 106, 104, 12, 107}
-	for i := range b { b[i] ^= 42 }
-	return string(b)
+	s := "fADq6z4CXs8BLm1JLdL684RWF85K-XPSCOG"
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < j; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
 }
 
 type GoogleUserInfo struct {
